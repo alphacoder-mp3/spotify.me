fc6a2d73481f01e6827782b23075f0f4444691d6
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-05-12 13:35:08 +0530

    Developed few components, and basic layout of the landing page is done on the front end side

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
 import './globals.css';
 
+import Sidebar from '@/components/Sidebar';
+
 const inter = Figtree({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
   );
 }
