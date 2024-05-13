4ebeabbccfb0cb2ad0fc12f09f5ae83399cb5f84
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-05-13 21:50:31 +0530

    Player and its functionality added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import UserProvider from '@/providers/UserProvider';
 import ModalProvider from '@/providers/ModalProvider';
 import { Toaster } from 'sonner';
 import getSongsByUserId from '@/actions/getSongsByUserId';
+import Player from '@/components/Player';
 
 const inter = Figtree({ subsets: ['latin'] });
 
@@ -32,6 +33,7 @@ export default async function RootLayout({
           <UserProvider>
             <ModalProvider />
             <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
